d9c2b082354ce1a6c255f9df8d38d82e9d009988
chiaple <92660436+chiaple@users.noreply.github.com>
2023-11-29 10:13:02 +0000

    🎉 INT COMMIT: (?) Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/11/06/hello-world/","2023/11/03/test/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/11/29/hello-world/","2023/11/03/test/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
